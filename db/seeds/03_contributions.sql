fde4f2088e8c909085a0692fafbeb3372cc37a40
Anaaag <agarc20@outlook.com>
2022-10-15 20:11:54 -0700

    updated schema & seeds file

--- db/seeds/03_contributions.sql
+++ db/seeds/03_contributions.sql
@@ -1,26 +1,26 @@
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 1, 1);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 1, 1);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 2, 2);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 2, 2);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 3, 3);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 3, 3);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 4, 4);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 4, 4);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 5, 5);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 5, 5);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 6, 1);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 6, 1);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 7, 2);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 7, 2);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 8, 3);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 8, 3);
 
-INSERT INTO contributions (content, created, status, story_id, user_id)
-VALUES ('THIS CONFIRMS THAT THE CONTRIBUTION WAS ADDED', 0, FALSE, 9, 4);
+INSERT INTO contributions (created, story_id, user_id)
+VALUES ('2022-10-17', 9, 4);
